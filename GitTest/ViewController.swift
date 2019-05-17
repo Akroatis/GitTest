e84b1e547567c9e4ea7948cdaf858ab32f512a91
Patrick Stacey-Vargus <pstacey-vargus@Administrators-MacBook-Air-2.local>
2019-05-17 10:41:28 -0500

    test

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Patrick Stacey-Vargus on 5/17/19.
+//  Copyright © 2019 Patrick Stacey-Vargus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
